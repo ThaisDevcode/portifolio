fe0ee95fd1c23571ba7b16b8b2243ed374e3b094
Thaís Pereira <thaiis6813@gmail.com>
2026-08-19 22:34:50 -0300

    feat: cria portfólio

---
+++ projeto.js
@@ -0,0 +1,13 @@
+
+function ativaletra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i)=>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 75 * i);        
+    });
+}
+
+const titulo = document.querySelector('.digitando');
+ativaletra(titulo);
